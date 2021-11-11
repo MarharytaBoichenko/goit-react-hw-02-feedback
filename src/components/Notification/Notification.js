029e8088a02f19f771f5a5c2aed6d4a165b154d5
MarharytaBoichenko <boichenkomv@gmail.com>
2021-11-11 13:40:31 +0200

    save  isRequired

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -6,5 +6,5 @@ const Notification = ({ message }) => {
 export default Notification;
 
 Notification.propTypes = {
-  message: PropTypes.string,
+  message: PropTypes.string.isRequired,
 };
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -12,5 +12,5 @@ const Section = ({ title, children }) => {
 export default Section;
 
 Section.propTypes = {
-  title: PropTypes.string,
+  title: PropTypes.string.isRequired,
 };
